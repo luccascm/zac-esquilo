655d1bc842a6d05db4e4868610d047bea6f6b7b0
Lucas <cmedeiros.lucas@gmail.com>
2014-12-15 20:07:16 -0200

    redimensionando char

--- js/game/states/PlayGame.js
+++ js/game/states/PlayGame.js
@@ -5,8 +5,8 @@ ZacEsquilo.PlayGame = function(){
 ZacEsquilo.PlayGame.prototype = {
   create: function(){
     this.zac = this.add.sprite(this.game.world.centerX, 5, 'char');
-    this.zac.scale.x = 0.5;
-    this.zac.scale.y = 0.5;
+    this.zac.scale.x = 0.3;
+    this.zac.scale.y = 0.3;
     this.zac.anchor.setTo(0.5);
   }
 };
