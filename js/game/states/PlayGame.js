37e468fa0dec077d0f5596b80b40ac7d4c946062
Lucas <cmedeiros.lucas@gmail.com>
2014-12-17 16:44:00 -0200

    adicionando botao voltar

--- js/game/states/PlayGame.js
+++ js/game/states/PlayGame.js
@@ -4,7 +4,7 @@ ZacEsquilo.PlayGame = function(){
 
 ZacEsquilo.PlayGame.prototype = {
   create: function(){
-    this.zac = this.add.sprite(this.game.world.centerX, 5, 'char');
+    this.zac = this.add.sprite(this.game.world.centerX, this.game.world, 'char');
     this.zac.scale.x = 0.3;
     this.zac.scale.y = 0.3;
     this.zac.anchor.setTo(0.5);
