0c10dbdadc11729465eae0df67cec3a9772f2e7a
Lucas <cmedeiros.lucas@gmail.com>
2015-04-05 20:07:40 -0300

    Criando tela de scoreboard com possibilidade de recomeçar o jogo

--- js/game/states/Scoreboard.js
+++ js/game/states/Scoreboard.js
@@ -2,40 +2,71 @@
 
 // Copyright © 2015 Michael Dobekidis
 // Licensed under the terms of the MIT License
-var reg = {};
-ZacEsquilo.Scoreboard = function(game) { this.reg = {}; };
+ZacEsquilo.Scoreboard = function(game) { this.scoreText = null; };
 
 ZacEsquilo.Scoreboard.prototype = {
-  preload: function(){},
+  preload: function(){
+    this.game.add.tileSprite(0, 0, this.game.world.width, this.game.world.height, 'forestBackground');
+    
+    this.restartKey = this.input.keyboard.addKey(Phaser.Keyboard[ZacEsquilo.config.oneSwitchKey]);
+
+  },
 
   create: function(){
-    this.reg.modal = new gameModal(this.game);
-    this.createModals();
-    this.showModal1();
+    this.preload_char = this.add.sprite(30, this.game.world.height - 190, 'zacStart');
+    this.preload_char.scale = 0.4;
+
+    this.createText();
+
+    this.restartKey.onDown.add(this.restartGame,this);
+    // this.reg.modal = new gameModal(this.game);
+    // this.createModals();
+    // this.showModal1();
   },
 
-  createModals: function(){
-    this.reg.modal.createModal({
-      type:"modal1",
-      includeBackground: true,
-      modalCloseOnInput: true,
-      itemsArr: [
-        {
-          type: "text",
-          content: "Simple Text with Modal background, \n nothing fancy here...",
-          fontFamily: "Luckiest Guy",
-          fontSize: 38,
-          color: "0xFEFF49",
-          offsetY: -50,
-          stroke: "0x000000",
-          strokeThickness: 5
-        }
-      ]
-    });
+  createText: function(){
+    this.fontStyle = { font: "25px Bubblegum Sans", fill: "#fff", align: "center"};
+    this.scoreText = this.game.add.text(this.game.world.centerX, 100, "Parabéns! Você ajudou Zac a voltar para a floresta e venceu o jogo!", this.fontStyle);
+    this.scoreText.anchor.setTo(0.5);
+    this.scoreText.wordWrap = true;
+    this.scoreText.wordWrapWidth = this.game.world.width - 30;
+
+    this.playAgainText = this.game.add.text(this.game.world.centerX, 200, "Pressione espaço para jogar novamente", this.fontStyle);
+    this.playAgainText.anchor.setTo(0.5);
+    this.playAgainText.wordWrap = true;
+    this.playAgainText.wordWrapWidth = this.game.world.width - 30;
+
   },
 
-  showModal1: function(){
-    reg.modal.showModal("modal1");
+  update: function(){
+    
+  },
+
+  restartGame: function(key){
+    this.game.state.start('Boot');
   }
+  // createModals: function(){
+  //   this.reg.modal.createModal({
+  //     type:"modal1",
+  //     includeBackground: true,
+  //     modalCloseOnInput: true,
+  //     itemsArr: [
+  //       {
+  //         type: "text",
+  //         content: "Parabéns!! Você ajudou Zac a voltar para a floresta e venceu o jogo!",
+  //         fontFamily: "Luckiest Guy",
+  //         fontSize: 38,
+  //         color: "0xFEFF49",
+  //         offsetY: -50,
+  //         stroke: "0x000000",
+  //         strokeThickness: 5
+  //       }
+  //     ]
+  //   });
+  // },
+
+  // showModal1: function(){
+  //   this.reg.modal.showModal("modal1");
+  // }
 
 }
